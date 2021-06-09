dadfea892f080811e5e534641ac3a2e2f081df1f
Susan Adesoji <adesojisusan.adeola@gmail.com>
2021-06-09 11:18:12 +0100

    product schema

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify 
+from enum import unique
+from flask import Flask, config, request, jsonify 
 from  flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os
 
 
 app = Flask(__name__)
-
 basedir= os.path.abspath(os.path.dirname(__file__))
 #database
 app.config['SQLALCHEMY_DATABASE_URL']='sqlite:///'+ os.path.join(basedir, 'db.aqlite')
@@ -14,7 +14,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
 #init ma 
 ma = Marshmallow(app)
+#product class
+class Product(db.model):
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.column(db.String, unique = True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.column(db.Integer)
 
 
-if __name__ == '__main__':
+def __init__(self, name, description, price, qty):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.qty = qty
+
+
+#product schema 
+class ProductSchema(ma.schema):
+    class Meta:
+        fields = ('id', 'name', 'description','price' 'qty')
+
+#init schema
+
+product_schema = ProductSchema(strict = True)
+products_schema = ProductSchema(many = True, strict = True)
+
+
+
+
+
+
+
+if __name__ == '__main__': 
     app.run(debug=True)
