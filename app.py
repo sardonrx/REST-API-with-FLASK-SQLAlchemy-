0d056832f3bff2f6c3e1a96e7be1155a2c5181b7
Susan Adesoji <adesojisusan.adeola@gmail.com>
2021-06-09 16:16:28 +0100

    put product

--- app.py
+++ app.py
@@ -69,6 +69,28 @@ def get_product (id):
     product = Product.query.get(id)
     return product_schema.jsonify(product)
 
+#updating product 
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    update = Product.query.get(id)
+    
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json ['qty']
+
+    Product.name = name
+    Product.description = description
+    Product.price = price
+    Product.qty = qty
+
+    db.session.commit()
+    return product_schema.jsonify(update)
+
+
+
+
+
 
 
 
